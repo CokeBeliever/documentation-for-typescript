4ae89afdd8cf74aee82919dec440fd5a4b854d16
CokeBeliever <cokebeliever@163.com>
2022-11-06 23:07:26 +0800

    init project

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "TypeScript",
+  description: "TypeScript 中文文档",
+  lang: "zh-CN",
+  base: "/typescript",
+  themeConfig: {
+    outline: "deep",
+    nav: [
+      {
+        text: "官方文档",
+        link: "https://www.typescriptlang.org/",
+      },
+    ],
+    sidebar: [
+      {
+        text: "Get Started",
+        collapsible: true,
+        items: [
+          {
+            text: "TS for JS Programmers",
+            link: "/get-started/ts-for-js-programmers",
+          },
+        ],
+      },
+    ],
+    socialLinks: [{ icon: "github", link: "https://github.com/CokeBeliever" }],
+    editLink: {
+      pattern:
+        "https://github.com/CokeBeliever/documentation-for-typescript/edit/master/docs/:path",
+      text: "在 GitHub 编辑此页",
+    },
+    footer: {
+      copyright:
+        'Copyright © 2022-present <a href="https://github.com/CokeBeliever">CokeBeliever</a>',
+    },
+  },
+});
